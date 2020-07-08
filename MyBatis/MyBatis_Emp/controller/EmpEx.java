4eb28eabe12b2bacffa9698b38cdd293615a109b
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-07-08 16:58:29 +0900

    Update EmpEx.java

--- MyBatis/MyBatis_Emp/controller/EmpEx.java
+++ MyBatis/MyBatis_Emp/controller/EmpEx.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -12,16 +14,17 @@ import dto.Emp;
 import mybatis.MyBatisConnectionFactory;
 
 public class EmpEx {
+	//DB접속 및 SqlSession 생성 팩토리 객체
+	private static SqlSessionFactory factory = MyBatisConnectionFactory.getSqlSessionFactory();
+	
 	public static void main(String[] args) {		
 		//마이바티스 수행객체
 		SqlSession sqlSession;
-		//DB접속 및 SqlSession 생성 팩토리 객체
-		SqlSessionFactory factory = MyBatisConnectionFactory.getSqlSessionFactory();		
+				
 		//마이바티스 수행객체
 		sqlSession = factory.openSession(true); 
 		//마이바티스의 매퍼와 자바프로그램의 DAO 인터페이스 매핑(연결)
 		EmpDao empDao = sqlSession.getMapper(EmpDao.class);	
-		
 		
 		
 		//예제 퀴즈 실행
@@ -38,8 +41,19 @@ public class EmpEx {
 		System.out.println(">>사원번호 입력:");
 		emp.setEmpno(sc.nextInt());
 		System.out.println(">>부서comm 입력:");
-		emp.setComm(sc.nextInt());	
-		emp.setHiredate(new Date());
+		emp.setComm(sc.nextInt());
+		sc.nextLine();
+		System.out.println(">>부서 입사일 입력");
+		String input =sc.nextLine();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		Date hiredate=null;
+		try {
+			hiredate = sdf.parse(input);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		emp.setHiredate(hiredate);
+		
 		System.out.println(">>직업 입력:");
 		sc.nextLine();
 		emp.setJob(sc.nextLine());
@@ -51,11 +65,16 @@ public class EmpEx {
 		emp.setEname(sc.next());
 		
 		empDao.insertEmp(emp);
-		System.out.println("insert완료");
-		
+		System.out.println("insert완료");		
 		Emp emp2=empDao.selectByEmpNo(emp); 
 		System.out.println(emp2);
 		
+		// sqlSession에 기본적으로 한단계 더 높은 자동화 단계 - Mapper없이 바로가능
+//		sqlSession.insert(arg0); - int형 반환(Mapper에서는 반환처리할수없다)-여기서만가능
+//		sqlSession.select(arg0, arg1);
+//		sqlSession.selectOne()
+//		sqlSession.commit();
+//		sqlSession.rollback();
 		
 		
 		empDao.deleteByEmpNo(emp2);
@@ -69,6 +88,10 @@ public class EmpEx {
 		
 	}
 }
+
+
+
+
 
 
 
