f8c4650fedd055d8020c1316974a071822e09aa4
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-07-10 21:05:54 +0900

    Update DynamicQueryController.java

--- MyBatis/MyBatis_Web/dynamic/controller/DynamicQueryController.java
+++ MyBatis/MyBatis_Web/dynamic/controller/DynamicQueryController.java
@@ -39,16 +39,64 @@ public class DynamicQueryController extends HttpServlet {
 //		req.setAttribute("list", list);
 //		req.getRequestDispatcher("/WEB-INF/views/query.jsp").forward(req, resp);
 		
+		//--------------------------------------
 //		List res2 = dynamicQueryDao.select2("S");
 //		System.out.println(res2);
 		
-		Emp emp = new Emp();
-		emp.setEmpno(1111);
-		emp.setEname("");
+		//-----------------------------
+//		Emp emp = new Emp();
+//		emp.setEmpno(7369);
+//		emp.setEname("KING");
+//		
+//		List res3 = dynamicQueryDao.select3(emp);
+//		System.out.println(res3);
 		
-		List res3 = dynamicQueryDao.select3(emp);
-		System.out.println(res3);
+//		HashMap<String,Object>map = new HashMap<>();
+//		map.put("category", "ename"); //이름으로 검색을 누른상태
+//		map.put("keyword", "SMITH"); //smith가 입력됨
+//
+//		List<Emp>res4 = dynamicQueryDao.select4(map);
+//		System.out.println(res4);
+//		
+//		map.put("category", "job"); 
+//		map.put("keyword", "SALESMAN"); 
+//		
+//		List<Emp>res5 = dynamicQueryDao.select4(map);
+//		System.out.println(res5);
+		
+		
+		//----------------------------------------
+		req.getRequestDispatcher("/WEB-INF/views/queryCheckbox.jsp").forward(req, resp);		
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//전달값이 여러개일때는 values로 받아야한다
+		String[] data=req.getParameterValues("deptno");
+	
+		//배열같은 것은 파라미터로 바로 넣으면 않좋음 -> 해쉬맵을 넣어주자
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("deptnoArr", data);
+		List<Emp>list=dynamicQueryDao.selectCheckbox(map);
+		
+		for(Emp emp:list) {
+			System.out.println(emp);
+		}
 	}
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
