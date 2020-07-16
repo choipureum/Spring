8986434e6d65c5fdaafa590702ed89f42479b808
PuReum Choi <poo1994@hanmail.net>
2020-07-16 21:28:22 +0900

    Create DeptServiceImpl.java

---
+++ SpringFramework/SpringPractice/src/web/service/DeptServiceImpl.java
@@ -0,0 +1,25 @@
+package web.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import web.controller.DeptController;
+import web.dao.DeptDao;
+import web.dto.Dept;
+
+@Service
+public class DeptServiceImpl implements DeptService{
+	private Logger logger = LoggerFactory.getLogger(DeptServiceImpl.class);
+	@Autowired
+	private DeptDao deptDao;
+	
+	@Override
+	public List<Dept> getList() {
+		List<Dept> list = deptDao.selectAll();
+		return list;
+	}
+}
